dca7400dff6fe989b8971db600d73a6e0f350237
Kinjalsnovuscode <kinjal.s@novuscode.com>
2025-11-26 18:06:58 +0530

    Update settings to include template directory for enhanced email rendering capabilities.

--- crm/settings.py
+++ crm/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
